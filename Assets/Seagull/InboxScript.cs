48962b7bffb443961651a840ab48862f24f297be
kunalja <kunaljain@utexas.edu>
2025-09-14 17:50:29 -0400

    animate seagull to drop letter

--- Assets/Seagull/InboxScript.cs
+++ Assets/Seagull/InboxScript.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Playables;
 
 public class InboxScript : MonoBehaviour, IPointerClickHandler
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     public GameObject letter;
+    private PlayableDirector director;
+
+    void Awake()
+    {
+        // Get the component reference in Awake, which is guaranteed to run before OnEnable.
+        director = GetComponent<PlayableDirector>();
+    }
 
     void Start()
     {
@@ -18,10 +26,16 @@ public class InboxScript : MonoBehaviour, IPointerClickHandler
 
     }
 
+
     public void OnPointerClick(PointerEventData eventData)
     {
         // Your code to do something when the object is clicked
         Debug.Log("Object was clicked!", gameObject);
+        director.Play();
+    }
+
+    public void middleOfAnimation()
+    {
         letter.SetActive(true);
     }
 }
